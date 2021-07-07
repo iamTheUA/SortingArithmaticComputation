262f2856e06456519a974c44b912927a09558377
Ujjwal Aakash <ujjwalaakash46ua@gmail.com>
2021-07-07 16:46:02 +0530

    1st Arithmaic computation

--- SortingArithmaticComp.sh
+++ SortingArithmaticComp.sh
@@ -2,3 +2,7 @@ read -p "Enter First Number: " a
 read -p "Enter Second Number: " b
 read -p "Enter Third Number: " c
 
+#Doing Arithmatic calculation and Storing in Num varibles
+num1=$((a+(b*c)))
+
+
