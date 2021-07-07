d8245757aaa821b3491581fcbf4bbe63a9a63746
Ujjwal Aakash <ujjwalaakash46ua@gmail.com>
2021-07-07 16:51:11 +0530

    2nd Arithmatic calculation

--- SortingArithmaticComp.sh
+++ SortingArithmaticComp.sh
@@ -2,7 +2,9 @@ read -p "Enter First Number: " a
 read -p "Enter Second Number: " b
 read -p "Enter Third Number: " c
 
-#Doing Arithmatic calculation and Storing in Num varibles
+#Doing Arithmatic calculation and Storing in num varibles
 num1=$((a+(b*c)))
-
-
+echo $num1
+#Second Arithmathic calculation
+num2=$(((a*b)+c))
+echo $num2
